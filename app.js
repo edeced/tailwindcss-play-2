6bf74d424db931007d84338a0f417aca1b6e9e26
edeced <ed_ced@icloud.com>
2020-12-11 03:40:18 -0500

    adds list icons and css hidden login, no JS

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const playlist = document.getElementById('playlists')
+
+const yourLib = document.getElementById('yourLib')
+const login1 = yourLib.nextElementSibling
+
+const addPlaylist = document.getElementById('addPlaylist')
+const login2 = addPlaylist.nextElementSibling
+
+const likedSongs = document.getElementById('likedSongs')
+const login3 = likedSongs.nextElementSibling
+
+
+playlists.addEventListener('click', function (e) {
+    e.preventDefault()
+  
+   // if ( .value === '') { .className = '' } 
+
+   // else { console.log('sending to server') }
+   
+  })
